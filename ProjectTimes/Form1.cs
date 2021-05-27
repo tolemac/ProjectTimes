62fdd9678d33902d68b0cbf00b9a1d4b49ccb8ec
Javier Ros Moreno <jros@jros.org>
2021-05-27 20:15:26 +0200

    Add project files.

---
+++ ProjectTimes/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectTimes
+{
+    public partial class Form1 : Form
+    {
+        private readonly ILogger<Form1> _logger;
+
+        public Form1(ILogger<Form1> logger)
+        {
+            _logger = logger;
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _logger.LogInformation("Form1 {BusinessLayerEvent} at {dateTime}", "Started", DateTime.UtcNow);
+
+                // Perform Business Logic here 
+                // _business.PerformBusiness();
+
+                MessageBox.Show("Hello .NET Core 3.0 . This is First Forms app in .NET Core");
+
+                _logger.LogInformation("Form1 {BusinessLayerEvent} at {dateTime}", "Ended", DateTime.UtcNow);
+
+            }
+            catch (Exception ex)
+            {
+                //Log technical exception 
+                _logger.LogError(ex.Message);
+                //Return exception repsponse here
+                throw;
+
+            }
+        }
+    }
+}
---
+++ ProjectTimes/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ProjectTimes
+{
+    static class Program
+    {
+
+        public static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole())
+                .AddSingleton<Form1>();
+        }
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var services = new ServiceCollection();
+
+            ConfigureServices(services);
+
+            using (ServiceProvider serviceProvider = services.BuildServiceProvider())
+            {
+                var form1 = serviceProvider.GetRequiredService<Form1>();
+                Application.Run(form1);
+            }
+        }
+    }
+}
